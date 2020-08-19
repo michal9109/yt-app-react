4a3e2b8e8d709408615c1e9b38306a3424803ecd
Dominika Dyrcz-Ciesielska <ciesielski.michal@interia.eu>
2020-08-19 22:05:08 +0200

    update label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -17,7 +17,7 @@ const SearchBar = ({ onFormSubmit }) => {
     <div className='search-bar ui segment'>
       <form onSubmit={onSubmit} className='ui form'>
         <div className='field'>
-          <label>Video Search</label>
+          <label>Video search </label>
           <input type='text' onChange={onInputChange} value={term} />
         </div>
       </form>
